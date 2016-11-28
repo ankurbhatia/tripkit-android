235c247d4a77281a754db7adb977cb259d4d7a7e
Tin Tran <thuutin@gmail.com>
2016-11-28 17:25:54 +0700

    Parse id attr of bikePod

--- android-shared-lib/src/main/java/com/skedgo/android/common/model/BikePodStop.java
+++ android-shared-lib/src/main/java/com/skedgo/android/common/model/BikePodStop.java
@@ -3,10 +3,12 @@ package com.skedgo.android.common.model;
 public class BikePodStop extends Location {
   private final BikePod bikePod;
   private final ModeInfo modeInfo;
+  private final String id;
 
-  public BikePodStop(BikePod bikePod, ModeInfo modeInfo) {
+  public BikePodStop(BikePod bikePod, ModeInfo modeInfo, String id) {
     this.bikePod = bikePod;
     this.modeInfo = modeInfo;
+    this.id = id;
   }
 
   public BikePod getBikePod() {
@@ -15,5 +17,9 @@ public class BikePodStop extends Location {
 
   public ModeInfo getModeInfo() {
     return modeInfo;
+  }
+
+  public String id() {
+    return id;
   }
 }
