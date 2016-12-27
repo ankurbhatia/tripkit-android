ba52d9e6b6b12a96756ecd447b20ae74fdaf396d
Tin Tran <thuutin@gmail.com>
2016-12-27 19:46:57 +0700

    Add Hierachy deserializing

--- android-shared-lib/src/main/java/com/skedgo/android/common/util/Gsons.java
+++ android-shared-lib/src/main/java/com/skedgo/android/common/util/Gsons.java
@@ -4,10 +4,22 @@ import android.support.annotation.NonNull;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.skedgo.android.common.model.BikePodStop;
+import com.skedgo.android.common.model.CarParkStop;
+import com.skedgo.android.common.model.CarPodStop;
+import com.skedgo.android.common.model.CarRentalStop;
 import com.skedgo.android.common.model.GsonAdaptersBikePod;
 import com.skedgo.android.common.model.GsonAdaptersBooking;
 import com.skedgo.android.common.model.GsonAdaptersRealtimeAlert;
 import com.skedgo.android.common.model.Region;
+import com.skedgo.android.common.model.ScheduledStop;
+
+import java.lang.reflect.Type;
 
 public final class Gsons {
   private Gsons() {}
@@ -19,6 +31,26 @@ public final class Gsons {
         .registerTypeAdapterFactory(new GsonAdaptersBooking())
         .registerTypeAdapterFactory(new GsonAdaptersRealtimeAlert())
         .registerTypeAdapterFactory(new GsonAdaptersBikePod())
+        .registerTypeHierarchyAdapter(ScheduledStop.class, new JsonDeserializer<BikePodStop>() {
+          @Override
+          public BikePodStop deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject jsonObject = json.getAsJsonObject();
+            String klass = jsonObject.get("class").getAsString();
+            switch (klass) {
+              case "CarPod":
+                return context.deserialize(json, CarPodStop.class);
+              case "CarPark":
+                return context.deserialize(json, CarParkStop.class);
+              case "BikePod":
+                return context.deserialize(json, BikePodStop.class);
+              case "CarRental":
+                return context.deserialize(json, CarRentalStop.class);
+              default:
+
+            }
+            return null;
+          }
+        })
         .create();
   }
 
