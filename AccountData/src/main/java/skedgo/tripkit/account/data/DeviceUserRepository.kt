d97ffae389f3b8dd210554f30a1f90e2c3fa16a3
Emmanuel Lagarrigue <emmanuel.lagarrigue@black-tobacco.com>
2018-02-21 13:56:10 -0300

    Update user account get

--- AccountData/src/main/java/skedgo/tripkit/account/data/DeviceUserRepository.kt
+++ AccountData/src/main/java/skedgo/tripkit/account/data/DeviceUserRepository.kt
@@ -1,6 +1,7 @@
 package skedgo.tripkit.account.data
 
 import android.accounts.AccountManager
+import android.annotation.SuppressLint
 import rx.Observable
 import rx.schedulers.Schedulers
 import skedgo.tripkit.account.domain.User
@@ -9,12 +10,15 @@ import skedgo.tripkit.account.domain.UserRepository
 internal class DeviceUserRepository constructor(
     private val accountManager: AccountManager
 ) : UserRepository {
+  @SuppressLint("MissingPermission")
   override fun getUser(): Observable<User> {
     return Observable
         .fromCallable {
-          accountManager.getAccountsByType("com.google").first()
+          accountManager.accounts.first()
         }
-        .map { User(it.name, it.type) }
+        .map {
+          User(it.name, it.type)
+        }
         .subscribeOn(Schedulers.io())
   }
 }
